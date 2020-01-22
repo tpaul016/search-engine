3d025b9ea0e9195baf5316d0366588c16bbd89bc
Raymond <raymondkenchang@gmail.com>
2020-01-22 16:15:44 -0500

    Start of UI

--- searchapp/searchapp.py
+++ searchapp/searchapp.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    query = ''
+    model = ''
+    collection = ''
+    return render_template('index.html', query=query)
+
+@app.route('/docs/docId')
+def get_document(docId):
+    return docId
+
