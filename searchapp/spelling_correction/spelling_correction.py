5a002e5adf0261bc591a7d1f7fb8432cfbc3bede
Tristan Paul <31858343+tpaul016@users.noreply.github.com>
2020-02-15 01:26:41 -0500

    Spelling correction (#19)
    * added wildcard expansion for boolean retrieval model
    
    * changed spelling suggestions display format

--- searchapp/spelling_correction/spelling_correction.py
+++ searchapp/spelling_correction/spelling_correction.py
@@ -9,11 +9,40 @@ import re
 insert_costs = np.ones(128, dtype=np.float64)
 
 # inspired by https://docs.python.org/3/library/heapq.html#heapq.nsmallest
-def check_spelling(input, N):
+def check_spelling(input, N, model):
     path_to_index = os.path.join(os.getcwd(), 'searchapp','index_and_dict', 'index.json')
+    # path_to_index = os.path.join(os.getcwd(), '..','index_and_dict', 'index.json')
     index = indexAccess.getInvertedIndex(path_to_index=path_to_index)
-    h = construct_heap(index, input);
-    return [heapq.heappop(h)[1] for i in range(N)]
+
+    input_terms = input.split(' ')
+    mispelled_term = False
+    for term in input_terms:
+        if term not in index:
+            mispelled_term = True
+
+    if not mispelled_term:
+        return []
+
+    print("'" + input + "'" + ' not in index')
+    suggestions = []
+    for term in input_terms:
+        if term not in index:
+            h = construct_heap(index, term)
+            suggestions.append([heapq.heappop(h)[1] for i in range(N)])
+        else:
+            suggestions.append([term for i in range(N)])
+
+    formatted_suggestions = format_suggestions(suggestions, len(input_terms), N)
+    return formatted_suggestions
+
+def format_suggestions(suggestions, rows, cols):
+    formatted_sugestions = []
+    for j in range(cols):
+        suggestion = ''
+        for i in range(rows):
+            suggestion += suggestions[i][j] + ' '
+        formatted_sugestions.append(suggestion)
+    return formatted_sugestions
 
 # inspired by https://docs.python.org/3/library/heapq.html#heapq.nsmallest
 def construct_heap(index, input):
