9a7c155a14d8d7132fa893adad447f1f64f10654
Raymond <raymondkenchang@gmail.com>
2020-04-11 00:52:36 -0400

    Add comments and fix file path

--- searchapp/knn/knn.py
+++ searchapp/knn/knn.py
@@ -8,12 +8,17 @@ import nltk
 import json
 
 def knn():
+    """
+        Classify all of our documents
+        Writes to file each document and its classified or already set topic
+    """
     path = "searchapp/cor_pre_proc/reuters/processed/"
     currDir = getcwd()
     chdir(path)
     fileNameList = listdir()
     chdir(currDir)
 
+    # Seperate files into testing (no topic) and training (has topic) sets
     training_set = []
     testing_set = []
     for f in fileNameList:
@@ -24,6 +29,7 @@ def knn():
         else:
             training_set.append(doc_id)
 
+    # Classify all documents in testing_set
     topics_map = {}
     with Pool() as pool:
         one_doc = pool.map(classify, testing_set, 30)
@@ -41,36 +47,52 @@ def knn():
         else:
             topics_map[doc] = [topic]
 
-    with open("classified_docs.json", 'w') as f:
+    with open("searchapp/knn/classified_docs.json", 'w') as f:
         json.dump(topics_map, f, indent=2, sort_keys=True)
 
 
 def classify(f):
+    """
+        Classify one document
+    """
     query = ""
     body = corpusAccess.getContentsReuters(f)
     tokens = nltk.word_tokenize(body)
     for token in tokens:
+        # Apply same preprocessing as inverted index
         token, ok = ind.preprocToken(token, True, True, True)
         if not ok:
             continue
         query += " " + token
 
-    ranking = vsm.rank(query, 5, corpus_enum.Corpus.REUTERS, True)
+    # Input body of our document as the query to VSM 
+    # to get the similarity to every other document
+    # 
+    # need_topics is set to True to ensure VSM only 
+    # ranks documents with topics (in our training set)
+    #
+    # We ask for the top 5 documents (k = 5)
+    ranking = vsm.rank(query, 5, corpus_enum.Corpus.REUTERS, True, [])
 
     cand_topics = []
+    # Get all the topics from our 5 documents
     for doc in ranking:
         topic = corpusAccess.getTopicsReuters(doc["docId"])
         if topic is None:
             continue
+        # We get the topics as a string
+        # Must split on comma to get each topic
         if "," in topic:
             new_cand_topics = topic.split(",")
             cand_topics += new_cand_topics
         else:
             cand_topics.append(topic)
 
-    # Select the most common topics
+    # From all of our topics take the topic that occurs the most
     c = Counter(cand_topics)
     tuple_topics = c.most_common(1)
+    # The list comprehension is if we wanted to 
+    # classify our documents with more then one topic
     topics = [t for t, count in tuple_topics]
     return f, topics
 
