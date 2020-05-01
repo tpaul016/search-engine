94c966434f9dfb1abddd3dc1dacc966c147eece5
Raymond <raymondkenchang@gmail.com>
2020-05-01 17:37:53 -0400

    Added more test cases, parameterized test

--- tests/test_vsm.py
+++ tests/test_vsm.py
@@ -1,4 +1,5 @@
 import json
+import pytest
 
 
 def check_query_response(response, valid):
@@ -13,35 +14,78 @@ def check_query_response(response, valid):
     return True
 
 
-def test_no_options(client):
+course_test_data = [
+    ("operating system", [{"docId": "csi3131",
+                           "score": 0.9999720660827457},
+                          {"docId": "adm2372",
+                           "score": 0.9952016502652135},
+                          {"docId": "csi4139",
+                           "score": 0.9952016502652133},
+                          {"docId": "csi5175",
+                           "score": 0.9952016502652133},
+                          {"docId": "adm6281",
+                           "score": 0.9717264505346447},
+                          {"docId": "csi5124",
+                           "score": 0.9717264505346447},
+                          {"docId": "mat2324",
+                           "score": 0.9717264505346447},
+                          {"docId": "mat2384",
+                           "score": 0.9717264505346447},
+                          {"docId": "mat5162",
+                           "score": 0.9717264505346447},
+                          {"docId": "csi5312",
+                           "score": 0.9226550151770498}]),
+    ("computers graphical", [{"docId": "csi4130",
+                              "score": 0.8610932492553997},
+                             {"docId": "adm4379",
+                              "score": 0.7071067811865476},
+                             {"docId": "adm3322",
+                              "score": 0.7071067811865475},
+                             {"docId": "adm3378",
+                              "score": 0.7071067811865475},
+                             {"docId": "adm4377",
+                              "score": 0.7071067811865475},
+                             {"docId": "adm5300",
+                              "score": 0.7071067811865475},
+                             {"docId": "adm6301",
+                              "score": 0.7071067811865475},
+                             {"docId": "adm6302",
+                              "score": 0.7071067811865475},
+                             {"docId": "csi1306",
+                              "score": 0.7071067811865475},
+                             {"docId": "csi1308",
+                              "score": 0.7071067811865475}]),
+    ("bayesian network classification", [{"docId": "mat3373",
+                                          "score": 0.8160587166375239},
+                                         {"docId": "adm3308",
+                                          "score": 0.8105812401314181},
+                                         {"docId": "mat4373",
+                                          "score": 0.8070819324373376},
+                                         {"docId": "csi5149",
+                                          "score": 0.8070819324373375},
+                                         {"docId": "csi5155",
+                                          "score": 0.8021696888570387},
+                                         {"docId": "mat5176",
+                                          "score": 0.5773502691896258},
+                                         {"docId": "mat5190",
+                                          "score": 0.5773502691896258},
+                                         {"docId": "mat5191",
+                                          "score": 0.5773502691896258},
+                                         {"docId": "mat5375",
+                                          "score": 0.5773502691896258},
+                                         {"docId": "adm3378",
+                                          "score": 0.5773502691896258}])]
+
+
+@pytest.mark.parametrize("query, valid", course_test_data)
+def test_no_options_courses(query, valid, client):
     rv = client.post('/docs', data=dict(
-        query="operoting system",
+        query=query,
         model="vsm",
         collection="courses",
         globExpan="",
         topic=""))
     response = json.loads(rv.data)["docs"]
-
-    valid = [{"docId": "psy6002",
-              "score": 0.882426459203083},
-             {"docId": "adm1340",
-              "score": 0.7071067811865475},
-             {"docId": "adm2372",
-              "score": 0.7071067811865475},
-             {"docId": "adm3301",
-              "score": 0.7071067811865475},
-             {"docId": "adm3345",
-              "score": 0.7071067811865475},
-             {"docId": "adm3346",
-              "score": 0.7071067811865475},
-             {"docId": "adm3360",
-              "score": 0.7071067811865475},
-             {"docId": "adm3363",
-              "score": 0.7071067811865475},
-             {"docId": "adm3378",
-              "score": 0.7071067811865475},
-             {"docId": "adm3379",
-              "score": 0.7071067811865475}]
     assert check_query_response(response, valid)
 
 
